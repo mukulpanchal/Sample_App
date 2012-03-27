311de5c87a149b07a2b7cb8a1bd3d2d0b79f27ed
Mukul Panchal <mukulpanchal@gmail.com>
2012-03-26 17:14:38 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Sample Application in Ruby on Rails
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '8395d68b7f41ba71ff8aa82ec4d0d2a4e41c825c5042087d3e28b88b3250a6692fec21a2b8a81d3baf9be5124cc9dba6026df780c04e35f3bbfe0ea4d0c45199'
