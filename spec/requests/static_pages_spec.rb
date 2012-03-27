7a9e852288fd7e9d1d294fc31405f21dbdec9b13
Mukul Panchal <mukulpanchal@gmail.com>
2012-03-26 22:41:14 -0700

    Finish contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -42,4 +42,18 @@ describe "Static pages" do
     end
   end
     
+  describe "Contact page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/contact'
+      page.should have_content('Sample App')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',:text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+    
+  end
+    
 end
